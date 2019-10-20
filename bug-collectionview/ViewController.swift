b156f722392393011d964a2a18d653780100d996
Eric <eric@skrobs.com>
2019-10-20 14:38:09 +0200

    Initial Commit

---
+++ bug-collectionview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bug-collectionview
+//
+//  Created by Eric Degrange on 20/10/2019.
+//  Copyright © 2019 Nothing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
